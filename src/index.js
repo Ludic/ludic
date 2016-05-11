165f687d69d99f5143ae2fdcc4c74ed3af53df50
Derek Arner <doman412@gmail.com>
2016-05-10 20:34:15 -0700

    changed some names

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import Util from './components/util/util';
+import D3App from './d3/D3App';
+
+
+var config = {
+  canvas: {
+    fullscreen: true
+  },
+  camera: {
+    fps: true,
+    extras: {
+      axes: true,
+      grid: false
+    },
+    ptm: 25
+  },
+  world: {
+    gravity: {
+      x: 0,
+      y: -10
+    },
+    drawDebug: true
+  },
+  input: {
+    logAllKeys:false,
+    logUnmappedKeys:true,
+  }
+};
+
+Util.setConfig(config);
+
+
+
+
+var app = new D3App();
+app.run();
