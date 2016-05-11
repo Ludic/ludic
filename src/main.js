140689f47fa98aff5cb0fe95961220dfc06330f7
Derek Arner <doman412@gmail.com>
2016-05-10 21:06:20 -0700

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 
-import ludic from './components/app/ludic'
+// import ludic from './components/app/ludic'
 // import BaseApp from './components/app/BaseApp'
-import ImageAsset from './components/base/imageAsset'
+// import ImageAsset from './components/base/imageAsset'
+// console.log(ludic);
+// console.log(ImageAsset);
 
-console.log(ludic);
-console.log(ImageAsset);
+
+import Box2D from './components/box2d/box2d'
 
 //
 // let _Module = {
