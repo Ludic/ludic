72c7bf0aa92789704223db66aeb40196f6fa810b
Derek Arner <doman412@gmail.com>
2016-05-10 22:29:11 -0700

    test

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ System.config({
     "optional": [
       "runtime",
       "optimisation.modules.system"
-    ]
+    ],
+    "ignore": "Ludic/src/libs/box2d/2.3.1/Box2D_v2.3.1_min"
   }
   paths: {
     "github:*": "jspm_packages/github/*",
