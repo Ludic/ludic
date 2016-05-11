c91a52f2eb0d1b14fe1a5a8f151924600e08d78f
Derek Arner <doman412@gmail.com>
2016-05-10 20:57:29 -0700

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 
-// import ludic from './components/app/ludic'
+import ludic from './components/app/ludic'
 // import BaseApp from './components/app/BaseApp'
 
+console.log(ludic);
 
-let _Module = {
-  cheese: 'moz'
-};
-
-export default _Module;
+//
+// let _Module = {
+//   cheese: 'moz'
+// };
+//
+// export default _Module;
