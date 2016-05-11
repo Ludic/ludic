55edb89aecda70ce2d60bf82e3a2c872dbc5d0ee
Derek Arner <doman412@gmail.com>
2016-05-10 21:32:46 -0700

    test

--- src/components/box2d/box2d.js
+++ src/components/box2d/box2d.js
@@ -1,5 +1,6 @@
 // var Box2D = require('../../libs/box2d/2.3.1/Box2D_v2.3.1_min.js');
-import Module from '../../libs/box2d/2.3.1/Box2D_v2.3.1_min.js';
+import { Box2D } from '../../libs/box2d/2.3.1/Box2D_v2.3.1_min.js';
 // if (!Box2D) Box2D = (typeof Box2D !== 'undefined' ? Box2D : null) || Module;
+console.log(Box2D);
 
-export default Module.Box2D;
+export default Box2D;
