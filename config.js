ab6ffaeae9d5b8d4bf610402bf25e83fb30b7219
Derek Arner <doman412@gmail.com>
2016-05-10 18:33:53 -0700

    update package for box2d reqs

--- config.js
+++ config.js
@@ -17,7 +17,9 @@ System.config({
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
+    "fs": "github:jspm/nodelibs-fs@0.1.2",
     "jquery": "github:components/jquery@2.1.4",
+    "path": "github:jspm/nodelibs-path@0.1.0",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
--- src/components/box2d/box2d.js
+++ src/components/box2d/box2d.js
@@ -1,4 +1,5 @@
-var Box2D;
-if (!Box2D) Box2D = (typeof Box2D !== 'undefined' ? Box2D : null) || Module;
+// var Box2D = require('../../libs/box2d/2.3.1/Box2D_v2.3.1_min.js');
+import Module from '../../libs/box2d/2.3.1/Box2D_v2.3.1_min.js';
+// if (!Box2D) Box2D = (typeof Box2D !== 'undefined' ? Box2D : null) || Module;
 
-export default Box2D;
+export default Module.Box2D;
