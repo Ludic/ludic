99b0939fe887fc5c327ad626eae8c0947220b825
Derek Arner <doman412@gmail.com>
2016-05-10 20:24:36 -0700

    update config

--- config.js
+++ config.js
@@ -6,6 +6,9 @@ System.config({
     "optional": [
       "runtime",
       "optimisation.modules.system"
+    ],
+    "ignore": [
+      "src/libs/box2d/2.3.1/Box2D_v2.3.1_min.js"
     ]
   },
   paths: {
