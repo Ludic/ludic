9f5010c01d504233cd92ccbaaf67a9b4a526c36b
Derek Arner <doman412@gmail.com>
2016-05-12 20:29:05 -0700

    set up the exports

--- src/main.js
+++ src/main.js
@@ -1,19 +1,49 @@
 
-// import ludic from 'Ludic/src/components/app/ludic'
-// import BaseApp from './components/app/BaseApp'
-// import ImageAsset from './components/base/imageAsset'
-// console.log(ludic);
-// console.log(ImageAsset);
+// app
+export * from './components/app/BaseApp'
+export {Ludic as ludic} from './components/app/ludic'
+window.ludic = ludic; // put ludic on the window
 
+// base
+export * from './components/base/asset'
+export * from './components/base/assetManager'
+export * from './components/base/camera'
+export * from './components/base/fps'
+export * from './components/base/imageAsset'
+export * from './components/base/rubeAsset'
+export * from './components/base/rubeImageAsset'
 
-// import Box2D from './components/box2d/box2d'
-// console.log(Box2D);
+// box2d
+export * from './components/box2d/debugDraw'
 
-export * from './components'
+// canvas
+export * from './components/canvas/canvas'
 
-//
-// let _Module = {
-//   cheese: 'moz'
-// };
-//
-// export default _Module;
+// engine
+export * from './components/engine/BaseEntity'
+export * from './components/engine/CircleEntity'
+export * from './components/engine/EntityManager'
+export * from './components/engine/PolygonEntity'
+export * from './components/engine/StatefulPolygonEntity'
+
+// input
+export * from './components/input/inputController'
+
+// rube
+export * from './components/rube/RubeLoader'
+export * from './components/rube/RubeScene'
+
+// screen
+export * from './components/screen/screen'
+export * from './components/screen/screenManager'
+
+// sprite
+export * from './components/sprite/BaseSprite'
+export * from './components/sprite/MultiSprite'
+
+// util
+export * from './components/util/util'
+
+// world
+export * from './components/world/world'
+export * from './components/world/worldConfig'
