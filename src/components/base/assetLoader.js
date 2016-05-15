fd5ae939eb12fd1b5dbc054fd22d65749b60b309
Derek Arner <doman412@gmail.com>
2016-05-14 23:51:02 -0700

    this is working

---
+++ src/components/base/assetLoader.js
@@ -0,0 +1,11 @@
+
+
+export default class AssetLoader {
+  constructor() {
+
+  }
+
+  load(name, url, type options){
+    console.log('default loader', arguments);
+  }
+}
---
+++ src/components/base/imageAssetLoader.js
@@ -0,0 +1,13 @@
+import ImageAsset from './imageAsset';
+
+class ImageAssetLoader {
+  constructor() {
+
+  }
+
+  load(name, url, type, options){
+    return new ImageAsset(name, url, type, options);;
+  }
+}
+
+export default new ImageAssetLoader();
