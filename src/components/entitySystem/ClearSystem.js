fae9f5acda2324a3cfda6d61f44c32dde6e658a9
yintothayang <yintothayang@gmail.com>
2016-05-23 23:43:28 -0700

    added default color to ClearSystem

--- src/components/entitySystem/ClearSystem.js
+++ src/components/entitySystem/ClearSystem.js
@@ -1,25 +1,25 @@
 import BaseSystem from './BaseSystem'
 
 export default class ClearSystem extends BaseSystem {
-  constructor(canvas, active, priority){
+  constructor(canvas, color = "black", active = true, priority = -1){
     super(active, priority);
+    this.color = color;
     this.canvas = canvas;
     this.ctx = canvas.getContext();
   }
 
   //Overide
-  onEntityAdded(manager){
-  }
+  onEntityAdded(manager){}
 
-  onEntityRemoved(manager){
-  }
+  //Overide
+  onEntityRemoved(manager){}
 
   //Overide
   update(delta){
     this.ctx.restore();
-    this.ctx.fillStyle = "black";
-    this.ctx.clearRect(0, 0, this.canvas.width(), this.canvas.height() );
-    this.ctx.fillRect(0, 0, this.canvas.width(), this.canvas.height() );
+    this.ctx.fillStyle = this.color;
+    this.ctx.clearRect(0, 0, this.canvas.width(), this.canvas.height());
+    this.ctx.fillRect(0, 0, this.canvas.width(), this.canvas.height());
     this.ctx.save();
   }
 };
