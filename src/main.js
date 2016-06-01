16143d3e248cbbd2a9b0ac52f0e17324e954d868
Derek Arner <doman412@gmail.com>
2016-05-31 21:07:41 -0700

    remove all remnants of entity stuff to EiN

--- src/main.js
+++ src/main.js
@@ -10,22 +10,12 @@ export {default as AssetManager} from './components/base/assetManager'
 export {default as Camera} from './components/base/camera'
 export {default as fps} from './components/base/fps'
 export {default as ImageAsset} from './components/base/imageAsset'
-// export {default as RubeAsset} from './components/base/rubeAsset'
-// export {default as RubeImageAsset} from './components/base/rubeImageAsset'
-
-// box2d
-// export {default as DebugDraw} from './components/box2d/debugDraw'
 
 // canvas
 export {default as Canvas} from './components/canvas/canvas'
 
 // input
 export {default as InputController} from './components/input/inputController'
-export {default as InputSystem} from './components/input/InputSystem'
-
-// rube
-// export {default as RubeLoader} from './components/rube/RubeLoader'
-// export {default as RubeScene} from './components/rube/RubeScene'
 
 // screen
 export {default as Screen} from './components/screen/screen'
@@ -40,11 +30,3 @@ export {default as Util} from './components/util/util'
 
 // vectors
 export {default as Vector2} from './components/engine/Vector2'
-
-// world
-/* export {default as World} from './components/world/world'
-   export {default as WorldConfig} from './components/world/worldConfig' */
-
-// TODO maybe move these systems, into components/canvas and components/camera
-export {default as ClearSystem} from './components/entitySystem/ClearSystem'
-export {default as CameraSystem} from './components/entitySystem/CameraSystem'
