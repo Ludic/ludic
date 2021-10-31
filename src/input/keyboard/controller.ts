3d613b257eb726d9d994dd90c802145eec2e8ec4
Derek Arner <doman412@gmail.com>
2021-10-30 17:07:06 -0700

    add support for keyup/down flags for keyboard input

--- src/input/keyboard/controller.ts
+++ src/input/keyboard/controller.ts
@@ -20,47 +20,68 @@ const KEY_MAP = Object.fromEntries(KEYS.map((k, ix) => [k, ix]))
 
 export class KeyState {
   pressed: boolean
+  keyUp: boolean
+  keyDown: boolean
+}
+
+interface KeyboardSyncData {
+  name: string
+  data: {
+    buttons: Float64Array
+    lastButtons: Float64Array
+  }
 }
 
 export class KeyboardState extends InputState<KeyState> {
   private size: number
   private buttonsBuffer: ArrayBuffer
   private buttons: Float64Array
-  // private lastButtons: Float64Array
+  private lastButtons: Float64Array
   private syncToWorker: Worker|null
+  private keysDown = new Set()
+  private dirty = false
 
   constructor(){
     super({})
     this.size = 8
     
-    this.buttonsBuffer = new ArrayBuffer(KEYS.length*this.size)  // Length of KEYS * 8 bytes per key (64bit)
+    this.buttonsBuffer = new ArrayBuffer(KEYS.length*this.size*2)  // Length of KEYS * 8 bytes per key (64bit) * 2 (last half for last buttons)
 
     this.buttons = new Float64Array(this.buttonsBuffer, 0, KEYS.length)
-    // this.lastButtons = new Float64Array(this.buttonsBuffer, this.buttonsBuffer.byteLength/2)
+    this.lastButtons = new Float64Array(this.buttonsBuffer, this.buttonsBuffer.byteLength/2)
 
     if(typeof window === 'undefined'){
       // we are in a worker and should listen for events being passed from main thread
-      self.addEventListener('message', ({data})=>{
+      self.addEventListener('message', ({data}: MessageEvent<KeyboardSyncData>)=>{
         if(data){
           if(data.name === `ludic:input:keyboard:sync`){
             this.buttons.set(data.data.buttons)
+            this.lastButtons.set(data.data.lastButtons)
           }
         }
       })
     }
   }
 
-  set(key: number, val: boolean){
-    const value = this.buttons[key] = val ? 1 : 0
+  set(keyId: number, val: boolean){
+    val ? this.keysDown.add(keyId) : this.keysDown.delete(keyId)
+    this.dirty = true
+    const lastValue = this.lastButtons[keyId] = this.buttons[keyId]
+    const value = this.buttons[keyId] = val ? 1 : 0
     return {
       pressed: value == 1,
+      keyUp: value == 0 && lastValue == 1,
+      keyDown: value == 1 && lastValue == 0
     }
   }
   get(key: string|number){
     const keyId = typeof key === 'number' ? key : KEY_MAP[key]
     const value = this.buttons[keyId]
+    const lastValue = this.lastButtons[keyId]
     return {
       pressed: value == 1,
+      keyUp: value == 0 && lastValue == 1,
+      keyDown: value == 1 && lastValue == 0,
     }
   }
 
@@ -68,10 +89,21 @@ export class KeyboardState extends InputState<KeyState> {
     this.syncToWorker = worker
   }
 
+  update(){
+    this.sync()
+    if(this.keysDown.size === 0 && this.dirty){
+      this.dirty = false
+      this.lastButtons.fill(0)
+    } else if(this.keysDown.size > 0){
+      this.lastButtons.set(this.buttons)
+    }
+  }
+
   sync(){
     if(this.syncToWorker != null){
       Ludic.events.notify(`ludic:input:keyboard:sync`, {
         buttons: this.buttons,
+        lastButtons: this.lastButtons,
       }, this.syncToWorker)
     }
   }
@@ -100,8 +132,8 @@ export default class KeyboardController implements InputController {
       // this.state.set(code, { down })
     }
 
-    self.addEventListener('keydown', func, false)
-    self.addEventListener('keyup', func, false)
+    self.addEventListener('keydown', func, {capture: true, passive: true})
+    self.addEventListener('keyup', func, {capture: true, passive: true})
   }
   
   install(manager: InputManager){
@@ -113,6 +145,6 @@ export default class KeyboardController implements InputController {
   }
 
   update(){
-    this.state.sync()
+    this.state.update()
   }
 }
